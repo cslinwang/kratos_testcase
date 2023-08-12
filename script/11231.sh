c55e91b35d8ccc73ec4471c734b49dec24c03c39
yue ma <yuema@mail.dlut.edu.cn>
2023-08-12 17:23:10 +0000

    新增11231脚本

--- script/11231.sh
+++ script/11231.sh
@@ -4,7 +4,7 @@
 # https://github.com/KratosMultiphysics/Kratos/issues/11231
 
 # 预期报错：
-# ModuleNotFoundError: No module named 'swimming_DEM_procedures'
+# AssertionError: -1152.7766935045422 != -1219.77524 within 0.001 delta (66.99854649545773 difference)
 
 # 运行文件
 # /root/kratos_testcase/11231/11231.py
@@ -34,7 +34,7 @@ echo "Kratos编译完成。"
 # 运行
 echo "开始运行测试用例..."
 cd /root/kratos_testcase/11231
-python3 /root/kratos_testcase/11231/11231.py
+python3 /root/Kratos/applications/DEMApplication/tests/test_history_dependent_CLs.py
 echo "BUG测试用例运行完成。"
 
 # 生成覆盖率报告
