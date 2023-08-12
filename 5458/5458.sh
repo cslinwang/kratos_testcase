0b92a4b09e15acb1103f58595ad986d0b20f1f70
lin wang <wwwglin@163.com>
2023-08-12 14:10:59 +0000

    复现5458，增加覆盖率

---
+++ 5458/5458.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Bug url
+# https://github.com/KratosMultiphysics/Kratos/issues/5458
+
+# 预期报错：
+# ModuleNotFoundError: No module named 'swimming_DEM_procedures'
+
+# 运行文件
+# /root/Kratos/applications/SwimmingDEMApplication/tests/SmallTests.py
+
+bug_id=5458
+
+# 运行命令
+
+# 旧版
+
+# 编译
+# echo "开始编译Kratos..."
+# /root/kratos_testcase/script/7_kratos.sh 85619af239211663c59cbd403d7b3252df621990
+# echo "Kratos编译完成。"
+
+# # # 运行
+# echo "开始运行测试用例..."
+# python3 /root/Kratos/applications/SwimmingDEMApplication/tests/SmallTests.py
+# echo "测试用例运行完成。"
+
+# # 生成覆盖率报告
+# echo "开始生成覆盖率报告..."
+# /root/kratos_testcase/script/coverage.sh "$bug_id"
+# echo "覆盖率报告生成完成。"
+
+# 存档bug_id脚本
+echo "开始存档 BUG $bug_id 脚本..."
+cp "/root/kratos_testcase/script/$bug_id.sh" "/root/kratos_testcase/$bug_id/$bug_id.sh"
+echo "存档 BUG $bug_id 脚本完成。"
--- script/5458.sh
+++ script/5458.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Bug url
 # https://github.com/KratosMultiphysics/Kratos/issues/5458
 
@@ -6,6 +8,8 @@
 
 # 运行文件
 # /root/Kratos/applications/SwimmingDEMApplication/tests/SmallTests.py
+
+bug_id=5458
 
 # 运行命令
 
@@ -17,11 +21,16 @@ echo "开始编译Kratos..."
 echo "Kratos编译完成。"
 
 # 运行
-echo "开始运行测试用例..."
+echo "开始运行 BUG $bug_id 测试用例..."
 python3 /root/Kratos/applications/SwimmingDEMApplication/tests/SmallTests.py
-echo "测试用例运行完成。"
+echo "BUG $bug_id 测试用例运行完成。"
 
 # 生成覆盖率报告
-echo "开始生成覆盖率报告..."
-/root/kratos_testcase/script/coverage.sh 5458
-echo "覆盖率报告生成完成。"
+echo "开始生成 BUG $bug_id 覆盖率报告..."
+/root/kratos_testcase/script/coverage.sh "$bug_id"
+echo "BUG $bug_id 覆盖率报告生成完成。"
+
+# 存档bug_id脚本
+echo "开始存档 BUG $bug_id 脚本..."
+cp "/root/kratos_testcase/script/$bug_id.sh" "/root/kratos_testcase/$bug_id/$bug_id.sh"
+echo "存档 BUG $bug_id 脚本完成。"
