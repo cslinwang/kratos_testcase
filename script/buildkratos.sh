2a3068d2fbcb547df3631c6654e7107336ed2925
yue ma <yuema@mail.dlut.edu.cn>
2023-08-13 12:32:00 +0000

    更新buildkratos.sh

--- script/buildkratos.sh
+++ script/buildkratos.sh
@@ -23,11 +23,11 @@ export PYTHON_EXECUTABLE="/usr/bin/python3"
 # Set applications to compile
 export KRATOS_APPLICATIONS=
 # add_app ${KRATOS_APP_DIR}/LinearSolversApplication
-add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
-# add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+# add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
+add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
 # add_app ${KRATOS_APP_DIR}/FluidDynamicsBiomedicalApplication
-# add_app ${KRATOS_APP_DIR}/MeshingApplication
-add_app ${KRATOS_APP_DIR}/RomApplication
+add_app ${KRATOS_APP_DIR}/MeshingApplication
+# add_app ${KRATOS_APP_DIR}/RomApplication
 # add_app ${KRATOS_APP_DIR}/DEMApplication
 # add_app ${KRATOS_APP_DIR}/KratosMultiphysics.mpi
 
