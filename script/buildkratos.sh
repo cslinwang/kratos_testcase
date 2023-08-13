bcf3703d88f66b72233662b1a93b3699621aaed1
lin wang <wwwglin@163.com>
2023-08-13 12:36:24 +0000

    更新buildkratos.sh

--- script/buildkratos.sh
+++ script/buildkratos.sh
@@ -25,9 +25,11 @@ export KRATOS_APPLICATIONS=
 # add_app ${KRATOS_APP_DIR}/LinearSolversApplication
 # add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
 add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
+# add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
+add_app ${KRATOS_APP_DIR}/FluidDynamicsApplication
 # add_app ${KRATOS_APP_DIR}/FluidDynamicsBiomedicalApplication
 add_app ${KRATOS_APP_DIR}/MeshingApplication
-# add_app ${KRATOS_APP_DIR}/RomApplication
+# # add_app ${KRATOS_APP_DIR}/RomApplication
 # add_app ${KRATOS_APP_DIR}/DEMApplication
 # add_app ${KRATOS_APP_DIR}/KratosMultiphysics.mpi
 
