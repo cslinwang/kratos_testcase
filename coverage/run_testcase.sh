17f384f4349d3d0848f0c3c2e1a73f300b212ea8
lin wang <wwwglin@163.com>
2023-09-11 13:41:51 +0000

    增加python覆盖率

--- coverage/run_testcase.sh
+++ coverage/run_testcase.sh
@@ -42,10 +42,28 @@ fi
 # 执行测试命令
 echo "正在执行应用 ${application_name} 的测试: $python_script_path"
 cd /root/Kratos/applications/"$application_name"/tests
-python3 "$python_script_path"
-# 生成python覆盖率
-coverage xml -o "$current_directory/coverage.xml"
+# python3 "$python_script_path"
+# 生成python覆盖率，如果不存在，创建
+PYTHON_COVERAGE_PATH="$current_directory/python"
+if [ ! -d "$PYTHON_COVERAGE_PATH" ]; then
+  mkdir -p "$PYTHON_COVERAGE_PATH"
+fi
+# 清除覆盖率数据
+coverage erase
 
+coverage run --source=/root/Kratos/ "$python_script_path"
+
+# 生成文本覆盖率报告
+coverage report -m
+
+# 创建HTML覆盖率报告
+coverage html -d ${PYTHON_COVERAGE_PATH}
+
+# 创建JSON覆盖率报告
+coverage json -o ${PYTHON_COVERAGE_PATH}/coverage.json
+
+# 创建XML覆盖率报告
+coverage xml -o ${PYTHON_COVERAGE_PATH}/coverage.xml
 
 # 执行 fastcov 命令，将输出文件保存在当前目录下
 cd /root
