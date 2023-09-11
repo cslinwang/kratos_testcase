5dcdfe53f6410f44176d2b8ca67e06c8804d9543
lin wang <wwwglin@163.com>
2023-09-11 16:11:31 +0000

    10553新增py覆盖率

--- script/10553.sh
+++ script/10553.sh
@@ -36,7 +36,27 @@ echo "开始运行测试用例..."
 cd /root/Kratos/kratos/tests
 for i in $(seq 1 2000); do
     echo "Running iteration $i"
-    python3 /root/Kratos/kratos/tests/test_particles_utilities.py
+    PYTHON_COVERAGE_PATH=/root/kratos_testcase/10553/python
+    if [ ! -d "$PYTHON_COVERAGE_PATH" ]; then
+      mkdir -p "$PYTHON_COVERAGE_PATH"
+    fi
+    # 清除覆盖率数据
+    coverage erase
+
+    coverage run --source=/root/Kratos/ /root/Kratos/kratos/tests/test_particles_utilities.py
+
+    # 生成文本覆盖率报告
+    coverage report -m
+
+    # 创建HTML覆盖率报告
+    coverage html -d ${PYTHON_COVERAGE_PATH}
+
+    # 创建JSON覆盖率报告
+    coverage json -o ${PYTHON_COVERAGE_PATH}/coverage.json
+
+    # 创建XML覆盖率报告
+    coverage xml -o ${PYTHON_COVERAGE_PATH}/coverage.xml
+
     if [ $? -ne 0 ]; then
         echo "Error encountered on iteration $i. Stopping."
         break
