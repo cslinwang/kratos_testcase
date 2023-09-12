3336da6ad9577c3fd04a251681d505871b6e6647
lin wang <wwwglin@163.com>
2023-09-12 03:08:33 +0000

    3442新增py覆盖率

--- script/3442.sh
+++ script/3442.sh
@@ -36,7 +36,25 @@ echo "Kratos编译完成。"
 # 运行
 echo "开始运行测试用例..."
 cd /root/kratos_testcase/3442
-python3 /root/kratos_testcase/3442/MainKratos.py
+PYTHON_COVERAGE_PATH=/root/kratos_testcase/3442/python
+if [ ! -d "$PYTHON_COVERAGE_PATH" ]; then
+  mkdir -p "$PYTHON_COVERAGE_PATH"
+fi
+# 清除覆盖率数据
+coverage erase
+
+coverage run --source=/root/Kratos/ /root/kratos_testcase/3442/MainKratos.py
+coverage report -m
+
+# 创建HTML覆盖率报告
+coverage html -d ${PYTHON_COVERAGE_PATH}
+
+# 创建JSON覆盖率报告
+coverage json -o ${PYTHON_COVERAGE_PATH}/coverage.json
+
+# 创建XML覆盖率报告
+coverage xml -o ${PYTHON_COVERAGE_PATH}/coverage.xml
+
 echo "测试用例运行完成。"
 
 # 生成覆盖率报告
