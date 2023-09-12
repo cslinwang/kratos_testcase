1dd917960d92414b71040d8545012a54167656f5
lin wang <wwwglin@163.com>
2023-09-12 01:30:40 +0000

    9818新增py覆盖率

--- script/9818.sh
+++ script/9818.sh
@@ -4,7 +4,9 @@
 # https://github.com/KratosMultiphysics/Kratos/issues/9818
 
 # 预期报错：
-# it throws an error saying that the element base class is called
+# RuntimeError: Error: The following errors occured in a parallel region!
+# Thread #0 caught exception: Error: Calling base class ShapeFunctionsValues method instead of derived class one. Please check the
+# definition of derived class. 3 dimensional
 
 # 运行文件
 # /root/kratos_testcase/9818/run_kratos.py
@@ -30,7 +32,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Kratos/bin/Release/libs
 
 # 运行命令
 
-# 编译
+编译
 echo "开始编译Kratos..."
 /root/kratos_testcase/script/runkratos.sh 7fced4ec7d39daf577c4ba5573250b74f795b025
 echo "Kratos编译完成。"
@@ -38,7 +40,26 @@ echo "Kratos编译完成。"
 # 运行
 echo "开始运行测试用例..."
 cd /root/kratos_testcase/9818
-python3 /root/kratos_testcase/9818/run_kratos.py
+PYTHON_COVERAGE_PATH=/root/kratos_testcase/9818/python
+if [ ! -d "$PYTHON_COVERAGE_PATH" ]; then
+  mkdir -p "$PYTHON_COVERAGE_PATH"
+fi
+# 清除覆盖率数据
+coverage erase
+
+coverage run --source=/root/Kratos/ /root/kratos_testcase/9818/run_kratos.py
+
+# 生成文本覆盖率报告
+coverage report -m
+
+# 创建HTML覆盖率报告
+coverage html -d ${PYTHON_COVERAGE_PATH}
+
+# 创建JSON覆盖率报告
+coverage json -o ${PYTHON_COVERAGE_PATH}/coverage.json
+
+# 创建XML覆盖率报告
+coverage xml -o ${PYTHON_COVERAGE_PATH}/coverage.xml
 echo "BUG测试用例运行完成。"
 
 # 生成覆盖率报告
