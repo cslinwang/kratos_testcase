e54cfb2f07f50f4f61ec7d9f9ea29c2daae93844
yue ma <yuema@mail.dlut.edu.cn>
2023-09-12 06:36:44 +0000

    9079新增py覆盖率

--- script/9079.sh
+++ script/9079.sh
@@ -33,14 +33,34 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Kratos/bin/Release/libs
 # 运行命令
 
 # 编译
-echo "开始编译Kratos..."
-/root/kratos_testcase/script/runkratos.sh ed84ebfa2b2a207068492cdba47d9f306e9e0ef3
-echo "Kratos编译完成。"
+# echo "开始编译Kratos..."
+# /root/kratos_testcase/script/runkratos.sh ed84ebfa2b2a207068492cdba47d9f306e9e0ef3
+# echo "Kratos编译完成。"
 
 # 运行
 echo "开始运行测试用例..."
 cd /root/kratos_testcase/9079
-python3 /root/kratos_testcase/9079/9079.py
+PYTHON_COVERAGE_PATH=/root/kratos_testcase/9079/python
+if [ ! -d "$PYTHON_COVERAGE_PATH" ]; then
+  mkdir -p "$PYTHON_COVERAGE_PATH"
+fi
+# 清除覆盖率数据
+coverage erase
+
+coverage run --source=/root/Kratos/ /root/kratos_testcase/9079/9079.py
+
+# 生成文本覆盖率报告
+coverage report -m
+
+# 创建HTML覆盖率报告
+coverage html -d ${PYTHON_COVERAGE_PATH}
+
+# 创建JSON覆盖率报告
+coverage json -o ${PYTHON_COVERAGE_PATH}/coverage.json
+
+# 创建XML覆盖率报告
+coverage xml -o ${PYTHON_COVERAGE_PATH}/coverage.xml
+
 echo "BUG测试用例运行完成。"
 
 # 生成覆盖率报告
